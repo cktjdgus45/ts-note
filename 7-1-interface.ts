382f5d967f5b22ec366a48843a26fdcf7f7f53d3
pooh <cktjdgus45@naver.com>
2021-12-13 10:00:03 +0900

    type vs interface

--- 7-1-interface.ts
+++ 7-1-interface.ts
@@ -1,6 +1,13 @@
 {
     //TYPE ALIAS VS INTERFACE "언제 type을 쓰고 언제 interface를 써야 하는가." 
+    //Interface는 어떤 것의 규격사항이다. obj간에 의사소통을 할때 정해진 규격에의해서 사용하는 계약서이다.
+    //makeCoffee() 를 규격하고 그것을 따라간다.
+    //이런 경우에 type을 쓰는건 좋지 않다. 규격을 정의하고 그것을 따라가는 것이라면, interface
 
+    //type이란 데이터의 모습을 결정하는것
+    //position {x:num , y:num} 같은 경우는 type을 쓰는 것이 낫다.
+    //position을 구현하는 class가있는가? 아니라면 type.
+    //구현을 하는것이라면 interface , 데이터의 모습을 나타내는 것이라면 type alias.
     type PositionType = {
         x: number;
         y: number;
